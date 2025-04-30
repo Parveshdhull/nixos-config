080819e8c9b3cea59d7c5ea7300815617c7d524c
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2025-04-30 09:40:19 +0530

    Remove pam module

--- roles/desktop/yubikey.nix
+++ roles/desktop/yubikey.nix
@@ -29,18 +29,4 @@
     enable = true;
     enableSSHSupport = true;
   };
-
-  # Yubikey pam setup
-  security.pam.yubico = {
-    enable = true;
-    mode = "challenge-response";
-    id = [ (import "${secrets}/config").yubikey-new-serial ];
-  };
-
-  # Enable the u2f PAM module for login and sudo requests
-  # https://nixos.wiki/wiki/Yubikey#Logging-in
-  security.pam.services = {
-    # login.u2fAuth = true;
-    sudo.u2fAuth = true;
-  };
 }
