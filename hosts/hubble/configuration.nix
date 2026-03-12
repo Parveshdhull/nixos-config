69cbce69a7f6d0acb484c77ec92d3097a788d324
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2026-03-12 21:02:15 +0530

    fix hubble

--- hosts/hubble/configuration.nix
+++ hosts/hubble/configuration.nix
@@ -7,11 +7,26 @@
 
 {
   imports = [
-    ../../roles/base
+    ../../roles/base/basic-tools.nix
+    ../../roles/base/helpers.nix
+    ../../roles/base/janitor.nix
+    ../../roles/base/security.nix
     ../../roles/desktop/firejail.nix
     ../../roles/desktop/i3.nix
     ../../roles/desktop/thunar.nix
     ../../roles/wireguard/wireguard-temp.nix
     "${secrets}/hubble"
   ];
+
+  # base/default.nix
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.locate.enable = true;
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "24.05";
 }
